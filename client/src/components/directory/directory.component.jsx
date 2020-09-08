43e681e8f940f29be07fc5d20690213f339f2f2f
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-09-08 22:24:14 +0200

    Created header and sign-in component

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import Image1 from "./images/hats.png";
+import Image2 from "./images/jackets.png";
+import Image3 from "./images/sneakers.png";
+import Image4 from "./images/womens.png";
+import Image5 from "./images/mens.png";
 import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
@@ -12,35 +16,35 @@ class Directory extends React.Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl: Image1,
           id: 1,
-          linkUrl: "hats",
+          linkUrl: "shop/hats",
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl: Image2,
           id: 2,
-          linkUrl: "",
+          linkUrl: "shop/jackets",
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl: Image3,
           id: 3,
-          linkUrl: "",
+          linkUrl: "shop/sneakers",
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl: Image4,
           size: "large",
           id: 4,
-          linkUrl: "",
+          linkUrl: "shop/womens",
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl: Image5,
           size: "large",
           id: 5,
-          linkUrl: "",
+          linkUrl: "shop/mens",
         },
       ],
     };
