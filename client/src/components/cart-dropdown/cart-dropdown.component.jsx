bb8312644da4f5453edbb40b8456937c2d9f8f5e
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-09-10 18:56:45 +0200

    Used reselect for memoaization

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import CustomButton from "../custom-button/custom-button.component";
-import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -14,7 +17,9 @@ const CartDropdown = ({ cartItems }) => (
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
+
 export default connect(mapStateToProps)(CartDropdown);
