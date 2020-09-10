70ff9d0b831aef13eb42a0ab0774ad64ddd8c5cf
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-09-10 05:56:01 +0200

    Created cart-dropdown and cart-icon and cart-dropdown toggle

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -3,10 +3,11 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/star.svg";
-
+import CartIcon from "../cart-icon/cart-icon.component";
 import "./header.styles.scss";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -27,10 +28,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
+
 export default connect(mapStateToProps)(Header);
--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,9 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
