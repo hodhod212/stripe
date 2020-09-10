ef96f07c63d54910a99d23877fafaea0604d893b
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-09-10 02:25:41 +0200

    Implemented Redirect in signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import "./App.css";
 
@@ -45,15 +45,28 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 //a function some get dispatch property and return an object
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)), //user is payload
 });
-//first parameter is null because we don't need state in App.js we have got it in other places
-export default connect(null, mapDispatchToProps)(App);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/redux/user/user.reducer.js
+++ client/src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "./user.types";
 const INITIAL_STATE = {
   currentUser: null,
 };
+
 //if there is no state state = INITIAL_STATE
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
