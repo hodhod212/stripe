8d9ef189522fcfc4a5e927fc9d7330b0afd13e75
Abdullah <Abdullah@zenio.se>
2020-09-16 10:49:01 +0200

    done with clear cart

--- client/src/components/checkout-item/checkout-item.component.jsx
+++ client/src/components/checkout-item/checkout-item.component.jsx
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
+
 import React from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-=======
-import React from 'react';
-import { connect } from 'react-redux';
-import StripeCheckoutButton from "../stripe-button/stripe-button.component";
 
->>>>>>> delete-cart
 import {
   clearItemFromCart,
   addItem,
   removeItem,
-<<<<<<< HEAD
+
 } from "../../redux/cart/cart.actions";
-=======
-  clearCart
-} from '../../redux/cart/cart.actions';
->>>>>>> delete-cart
+
 
 import {
   CheckoutItemContainer,
@@ -25,14 +17,10 @@ import {
   TextContainer,
   QuantityContainer,
   RemoveButtonContainer,
-<<<<<<< HEAD
-} from "./checkout-item.styles";
-=======
-  ClearButton
-} from './checkout-item.styles';
->>>>>>> delete-cart
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem, onClearCart }) => {
+} from "./checkout-item.styles";
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   const qw = { quantity, price, name };
   axios
@@ -61,29 +49,16 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem, onClearCart })
     
   );
 };
-
-<<<<<<< HEAD
-const mapDispatchToProps = (dispatch) => ({
-  clearItem: (item) => dispatch(clearItemFromCart(item)),
-  addItem: (item) => dispatch(addItem(item)),
-  removeItem: (item) => dispatch(removeItem(item)),
-});
-
-export default connect(null, mapDispatchToProps)(CheckoutItem);
-=======
 const mapDispatchToProps = dispatch => ({
   clearItem: item => dispatch(clearItemFromCart(item)),
   addItem: item => dispatch(addItem(item)),
   removeItem: item => dispatch(removeItem(item)),
-  onClearCart: () => (dispatch(clearCart()))
+
 });
 // const mapDispatchToProps = (dispatch) => {
 //   return {
 //     onClearCart: () => (dispatch(clearCart()))
 //   }
 // };
-export default connect(
-  null,
-  mapDispatchToProps
-)(CheckoutItem);
->>>>>>> delete-cart
+export default CheckoutItem;
+
