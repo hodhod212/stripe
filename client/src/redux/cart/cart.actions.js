11268bdfd9a0f5718e327e6cf27e72d9fe716344
Abdullah <Abdullah@zenio.se>
2020-09-17 17:49:12 +0200

    Fix remove Iteam from cart

--- client/src/redux/cart/cart.actions.js
+++ client/src/redux/cart/cart.actions.js
@@ -10,7 +10,7 @@ export const addItem = (item) => ({
 });
 
 export const removeItem = (item) => ({
-  type: CartActionTypes.REMOVE_ITEMs,
+  type: CartActionTypes.REMOVE_ITEM,
   payload: item,
 });
 
